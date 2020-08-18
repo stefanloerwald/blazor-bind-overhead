2d7b94a564093950c72f93183f7ed20dc427fce1
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-08-18 12:00:57 +0200

    initial version of demo

---
+++ Components/Foo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Components
+{
+    public class Foo
+    {
+        public const int N = 60_000; // the number of elements we want to use in the demo
+        public int Value { get; set; }
+    }
+}
