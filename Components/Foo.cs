bf7a259cc24288cb5691bb6a2ebaf8e212fd6350
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-08-24 14:56:36 +0200

    new demo version, demonstrating the same issue with a much smaller number of components

--- Components/Foo.cs
+++ Components/Foo.cs
@@ -6,7 +6,7 @@ namespace Components
 {
     public class Foo
     {
-        public const int N = 60_000; // the number of elements we want to use in the demo
+        public const int N = 1000; // the number of elements we want to use in the demo
         public int Value { get; set; }
     }
 }
