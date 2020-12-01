f5f02258df3df059645566910716de3e14e2eafa
Stefan Lörwald <stefan.loerwald@gmail.com>
2020-12-01 11:35:44 +0100

    refactoring

--- Components/Foo.cs
+++ Components/Foo.cs
@@ -6,7 +6,6 @@ namespace Components
 {
     public class Foo
     {
-        public const int N = 1000; // the number of elements we want to use in the demo
         public int Value { get; set; }
     }
 }
